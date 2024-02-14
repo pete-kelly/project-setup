fedc74636068366a1092b2a973743a453cf079d8
Pete Kelly <petekelly22@gmail.com>
2024-02-14 09:36:12 -0500

    First commit

---
+++ project-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo
+echo "Starting project setup script ..."
+
+echo "Enter a name for the project ..."
+read "project_name"
+echo "Project name: $project_name"
+
+
+
+# create README
+#
+echo "Script complete"
+
